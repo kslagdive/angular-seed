7174b4ef06b13f8ef5abfb30ba4ef17cb214e3e5
kslagdive <kslagdive@gmail.com>
2017-01-10 19:43:35 +0530

    init commit

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -1,15 +1,43 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CanDeactivateGuard } from './can-deactivate-guard.service';
+import { AuthGuard }          from './auth-guard.service';
+import { PreloadSelectedModules } from './selective-preload-strategy';
+
+const appRoutes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: 'app/admin/admin.module#AdminModule',
+    canLoad: [AuthGuard]
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'crisis-center',
+    loadChildren: 'app/crisis-center/crisis-center.module#CrisisCenterModule',
+    data: {
+      preload: true
+    }
+  },
+];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot([
-      /* define app module routes here, e.g., to lazily load a module
-         (do not place feature module routes here, use an own -routing.module.ts in the feature instead)
-       */
-    ])
+    RouterModule.forRoot(
+      appRoutes,
+      { preloadingStrategy: PreloadSelectedModules }
+    )
   ],
-  exports: [RouterModule]
+  exports: [
+    RouterModule
+  ],
+  providers: [
+    CanDeactivateGuard,
+    PreloadSelectedModules
+  ]
 })
-export class AppRoutingModule { }
-
+export class AppRoutingModule {}
--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
 import { HttpModule } from '@angular/http';
+import { FormsModule }    from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -9,13 +11,32 @@ import { AboutModule } from './about/about.module';
 import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
 
+import { HeroesModule }         from './heroes/heroes.module';
+import { LoginRoutingModule }   from './login-routing.module';
+import { LoginComponent }       from './login.component';
+
+import { DialogService }        from './dialog.service';
+
 @NgModule({
-  imports: [BrowserModule, HttpModule, AppRoutingModule, AboutModule, HomeModule, SharedModule.forRoot()],
-  declarations: [AppComponent],
-  providers: [{
-    provide: APP_BASE_HREF,
-    useValue: '<%= APP_BASE %>'
-  }],
+  imports: [
+    BrowserModule, 
+    HttpModule, 
+    FormsModule,
+    AppRoutingModule, 
+    AboutModule, 
+    HomeModule, 
+    HeroesModule,
+    LoginRoutingModule,
+    SharedModule.forRoot()
+  ],
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  providers: [
+    DialogService,
+    
+  ],
   bootstrap: [AppComponent]
 
 })
